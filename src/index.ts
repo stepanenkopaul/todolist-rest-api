f102f6b56e050ae2c7e32daa07272904fe77ba06
Pavel Stepanenko <pavel.stepanenko@atotech.com>
2024-08-19 17:28:18 +0300

    changed addItem method

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const server = Fastify({
   logger: true
 });
 
+
+let items: Array<{ id: number, name: string }> = [
+  { id: 1, name: 'Item One' },
+  { id: 2, name: 'Item Two' }
+];
+
+
 server.register(cors, {
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE']
@@ -23,16 +30,23 @@ server.get('/items', { preHandler: KeycloakService.authorize }, async (request, 
     if (request.user == null){
       return {message: 'user unathorized'};
     }
-    const items = [{ id: 1, name: 'Item One' }, { id: 2, name: 'Item Two' }];
+    //const items = [{ id: 1, name: 'Item One' }, { id: 2, name: 'Item Two' }];
     return items;
 
   
 });
 
-server.post('/items', async (request, reply) => {
-  const newItem = request.body as Record<string, any>;
-  return { id: Math.random(), ...newItem };
+
+server.post('/items', { preHandler: KeycloakService.authorize }, async (request, reply) => {
+  console.log("items post");
+
+  const newItem = request.body as { name: string }; // Изменяем тип на более конкретный
+  const itemWithId = { id: Math.random(), name: newItem.name }; // Убедитесь, что оба свойства присутствуют
+  items.push(itemWithId); // Добавление нового элемента в массив items
+
+  return itemWithId;
 });
+
 
 const start = async () => {
   try {
